91d5af6a7dd6d24a29458927fd94760db07b8133
Piash Islam <piashislam2k@gmail.com>
2026-05-17 06:30:29 +0600

    added multiple way to variable declartion with var

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// isAdmin :=true   // short form does not work outside function
+
 func main() {
 	// var name string
 	// name = "Piash Islam"
@@ -10,12 +12,27 @@ func main() {
 
 	// name := "Piash Islam"
 
-	var (
-		name string = "Piash Islma"
-		age  int    = 26
-	)
+	//grouped variables
+	// var (
+	// 	name string = "Piash Islma"
+	// 	age  int    = 26
+	// )
+
+	//multiple variables declaration
+	// var x, y int
+	// x = 25
+	// y = 30
+
+	// var x, y int = 25, 30
+
+	var x, y string = "Piash ", "Islam"
+
+	isAdmin := true
 
 	// var name = "Piash Islam"
-	fmt.Println(name)
-	fmt.Println(age)
+	// fmt.Println(name)
+	// fmt.Println(age)
+
+	fmt.Println(x, y)
+	fmt.Println(isAdmin)
 }
